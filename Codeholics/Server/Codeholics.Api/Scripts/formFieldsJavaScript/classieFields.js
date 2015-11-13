525713ce78d417d341903931543ba70c9c7a067c
Pavlina Momchilova <pavlinalc@gmail.com>
2015-11-14 00:34:23 +0200

    form fields

--- Codeholics/Server/Codeholics.Api/Scripts/formFieldsJavaScript/classieFields.js
+++ Codeholics/Server/Codeholics.Api/Scripts/formFieldsJavaScript/classieFields.js
@@ -1,11 +1,11 @@
 ﻿/*!
- * classie - class helper functions
+ * classieFields - class helper functions
  * from bonzo https://github.com/ded/bonzo
  * 
- * classie.has( elem, 'my-class' ) -> true/false
- * classie.add( elem, 'my-new-class' )
- * classie.remove( elem, 'my-unwanted-class' )
- * classie.toggle( elem, 'my-class' )
+ * classieFields.has( elem, 'my-class' ) -> true/false
+ * classieFields.add( elem, 'my-new-class' )
+ * classieFields.remove( elem, 'my-unwanted-class' )
+ * classieFields.toggle( elem, 'my-class' )
  */
 
 /*jshint browser: true, strict: true, undef: true */
@@ -55,7 +55,7 @@
         fn(elem, c);
     }
 
-    var classie = {
+    var classieFields = {
         // full names
         hasClass: hasClass,
         addClass: addClass,
@@ -71,10 +71,11 @@
     // transport
     if (typeof define === 'function' && define.amd) {
         // AMD
-        define(classie);
+        define(classieFields);
     } else {
         // browser global
-        window.classie = classie;
+        window.classieFields = classieFields;
     }
 
 })(window);
+
--- Codeholics/Server/Codeholics.Api/Scripts/formFieldsJavaScript/textFieldJS.js
+++ Codeholics/Server/Codeholics.Api/Scripts/formFieldsJavaScript/textFieldJS.js
@@ -13,7 +13,7 @@
     [].slice.call(document.querySelectorAll('input.input__field')).forEach(function (inputEl) {
         // in case the input is already filled..
         if (inputEl.value.trim() !== '') {
-            classie.add(inputEl.parentNode, 'input--filled');
+            classieFields.add(inputEl.parentNode, 'input--filled');
         }
 
         // events:
@@ -22,12 +22,12 @@
     });
 
     function onInputFocus(ev) {
-        classie.add(ev.target.parentNode, 'input--filled');
+        classieFields.add(ev.target.parentNode, 'input--filled');
     }
 
     function onInputBlur(ev) {
         if (ev.target.value.trim() === '') {
-            classie.remove(ev.target.parentNode, 'input--filled');
+            classieFields.remove(ev.target.parentNode, 'input--filled');
         }
     }
 })();
